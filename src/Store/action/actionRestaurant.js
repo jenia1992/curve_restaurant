8b95e39de5c45fcf607797d00719825264444484
jenia1992 <33311012+jenia1992@users.noreply.github.com>
2020-03-06 18:33:43 +0200

    remove https

--- src/Store/action/actionRestaurant.js
+++ src/Store/action/actionRestaurant.js
@@ -11,7 +11,7 @@ export const RestaurantDataInit = (restaurants) => {
 export const dataCall = () => {
     return (dispatch) => {
         let data = {
-            url: "https://3.222.204.208/restaurants_list",
+            url: "http://3.222.204.208/restaurants_list",
             method: "post",
             // data: { "app_key": `${process.env.REACT_APP_CURVE_API_KEY}` },
             data:{"app_key":"ca0200ed-3d8f-417e-b4ec-da4f7e5546f5"},
@@ -22,7 +22,7 @@ export const dataCall = () => {
         getDataFromServer(data).then(res => {
             const menuPromises =res.data.restaurants.map(restaurant => {
                 let dataMenu = {
-                    url: "https://3.222.204.208/restaurant_menu",
+                    url: "http://3.222.204.208/restaurant_menu",
                     method: "post",
                     data: {
                         "app_key":"ca0200ed-3d8f-417e-b4ec-da4f7e5546f5",
