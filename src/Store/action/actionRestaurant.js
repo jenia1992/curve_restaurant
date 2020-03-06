53f6c9a0a307eccf4153da0e415e6014499e799c
jenia1992 <33311012+jenia1992@users.noreply.github.com>
2020-03-06 18:13:07 +0200

    add https

--- src/Store/action/actionRestaurant.js
+++ src/Store/action/actionRestaurant.js
@@ -11,7 +11,7 @@ export const RestaurantDataInit = (restaurants) => {
 export const dataCall = () => {
     return (dispatch) => {
         let data = {
-            url: "http://3.222.204.208/restaurants_list",
+            url: "https://3.222.204.208/restaurants_list",
             method: "post",
             data: { "app_key": `${process.env.REACT_APP_CURVE_API_KEY}` },
             headers: "",
@@ -21,7 +21,7 @@ export const dataCall = () => {
         getDataFromServer(data).then(res => {
             const menuPromises =res.data.restaurants.map(restaurant => {
                 let dataMenu = {
-                    url: "http://3.222.204.208/restaurant_menu",
+                    url: "https://3.222.204.208/restaurant_menu",
                     method: "post",
                     data: {
                         "app_key": `${process.env.REACT_APP_CURVE_API_KEY}`,
