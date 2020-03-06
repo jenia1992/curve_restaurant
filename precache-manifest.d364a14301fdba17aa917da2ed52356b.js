886ad326bd888b22d54705d45311a98aee8fd7cf
jenia1992 <33311012+jenia1992@users.noreply.github.com>
2020-03-06 18:07:39 +0200

    Updates

---
+++ precache-manifest.d364a14301fdba17aa917da2ed52356b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4fa2867671713e25272bc4fbb18c1b",
+    "url": "/curve_restaurant/index.html"
+  },
+  {
+    "revision": "5c6916264881ac9b5b99",
+    "url": "/curve_restaurant/static/css/main.a978f83e.chunk.css"
+  },
+  {
+    "revision": "52a39a0b0ff29ae0bc46",
+    "url": "/curve_restaurant/static/js/2.945bfa5e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/curve_restaurant/static/js/2.945bfa5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6916264881ac9b5b99",
+    "url": "/curve_restaurant/static/js/main.2ae997ef.chunk.js"
+  },
+  {
+    "revision": "0c9cef0f516c1d4a2229",
+    "url": "/curve_restaurant/static/js/runtime-main.91692255.js"
+  }
+]);
